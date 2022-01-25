7d676923a1fd3b06db47f8a5cfeb8ce7ecfac5f2
koomekevo <koome.kevo@gurucrafts.net>
2022-01-25 16:14:37 +0300

    Create empty-myprofile branch

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,27 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../logo.png';
 
-const Navbar = () => {
-  const r = '';
-  return (
-    <nav className='header-navbar'>
-      <div className='logo'>
-        <img
-          alt=""
-          src={logo}
-          width="50"
-          height="50"
-          className="header-logo"
-        />
-        {''}
-        <p className='space-travellers'>Space Traveler's Hub</p>
-</div>
-    </nav>
-  );
-};
+const Navbar = () => (
+  <nav className="header-navbar">
+    <div className="logo">
+      <img
+        alt=""
+        src={logo}
+        width="50"
+        height="50"
+        className="header-logo"
+      />
+
+      <p className="space-travellers">Space Travelers&apos Hub</p>
+    </div>
+    <div className="header-nav">
+      <ul className="header-nav_ul">
+        <li className="profile">
+          <NavLink to="/Profile" exact>My Profile</NavLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
 export default Navbar;
---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => (
+  <div>
+    <div className="profile-section">
+      <div className="my-missions-profile">
+        <h2 className="profile-headers">My Missions</h2>
+      </div>
+      <div className="my-rockets-profile">
+        <h2 className="profile-headers">My Rockets</h2>
+      </div>
+    </div>
+  </div>
+);
+
+export default Profile;
