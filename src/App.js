5e3199c0ed974243479b26678c6a0473e3fc4e6c
mohamedelshamy55 <mohamedelshamy55@gmail.com>
2022-01-26 12:36:20 +0200

    Fetch missions data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Header from './components/Header';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
 
 const App = () => (
   <Router>
-    <Header />
+    <div className="app">
+      <Header />
+      <Missions />
+      <Switch>
+        <Route exact path="/">
+          <Rockets />
+        </Route>
+        <Route path="/Missions">
+          <Missions />
+        </Route>
+        <Route path="/Profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </div>
   </Router>
 );
 
