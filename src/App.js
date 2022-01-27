2aa3035f320f03be8bf4276685c7eabded306497
koomekevo <koome.kevo@gurucrafts.net>
2022-01-27 17:01:31 +0300

    Fix app errors

--- src/App.js
+++ src/App.js
@@ -8,17 +8,19 @@ import Header from './components/Header';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
+import MissionsList from './pages/MissionsList';
 
 const App = () => (
   <Router>
     <div className="app">
       <Header />
+      <Missions />
       <Switch>
         <Route exact path="/">
           <Rockets />
         </Route>
-        <Route path="/">
-          <Missions />
+        <Route path="/Missions">
+          <MissionsList />
         </Route>
         <Route path="/Profile">
           <Profile />
